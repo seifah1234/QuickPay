9b0f0e54fe8405b181a596fd0a29badb9deaa7ec
Seif Amr PC <seifeldeen982005@gmail.com>
2026-08-09 16:51:35 +0300

    Add project files.

---
+++ QuickPay.DAL/QuickPayDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QuickPay.DAL
+{
+    public class QuickPayDbContext : DbContext
+    {
+
+        public QuickPayDbContext(DbContextOptions<QuickPayDbContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
