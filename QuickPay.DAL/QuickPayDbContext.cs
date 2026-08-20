b0bdefde96b01715cacae8d032046870a303877f
Roba <roba.ahmed211@gmail.com>
2026-08-20 05:23:12 +0300

    fix: moved connection string to .env

--- QuickPay.DAL/QuickPayDbContext.cs
+++ QuickPay.DAL/QuickPayDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using QuickPay.DAL.Entities;
 
 namespace QuickPay.DAL
--- QuickPay.DAL/QuickPayDbContextFactory.cs
+++ QuickPay.DAL/QuickPayDbContextFactory.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace QuickPay.DAL
 {
@@ -8,9 +10,17 @@ namespace QuickPay.DAL
         public QuickPayDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<QuickPayDbContext>();
+            
+            // Force-load the .env file explicitly for the design-time process
+            DotNetEnv.Env.Load(); // Or dotenv.net's DotEnv.Load();
 
-            optionsBuilder.UseSqlServer(
-                "Server=localhost;Database=QuickPayDB;Trusted_Connection=true;TrustServerCertificate=true;");
+            var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+            // 4. Build the DbContext options
+            optionsBuilder.UseSqlServer(connectionString);
+
+            //optionsBuilder.UseSqlServer(
+              //  "Server=localhost;Database=QuickPayDB;Trusted_Connection=true;TrustServerCertificate=true;");
 
             return new QuickPayDbContext(optionsBuilder.Options);
         }
